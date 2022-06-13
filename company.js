9fdee3dafd8008096a98f58eca787554fa914405
samdotkap <100087983+samdotkap@users.noreply.github.com>
2022-06-13 07:35:24 +0300

    updated milestone2

--- company.js
+++ company.js
@@ -1,3 +1,4 @@
+let loader2=document.querySelector(`.spinner-border2`);
 let container= document.querySelector(`.company-container`);
 let container2= document.querySelector(`.company-container2`);
 var urlParams = new URLSearchParams(window.location.search);
@@ -7,7 +8,7 @@ async function getSymbol(){
     try{
         const response = await fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${extractedSymbol}`)
         const data = await response.json()
-
+        
         let companyImg= data.profile.image
         let companyName=data.profile.companyName
         let companyDescrip=data.profile.description
@@ -23,7 +24,7 @@ async function getSymbol(){
         container2.innerHTML=`Stockprice: ${companyPrice} ${stockDifferential}%`
         
         // This isnt working. problem with color. IDK WHY
-        
+
         // if(stockDifferential>1){
         //     stockDifferential.style.color="green"
         // }else{
@@ -34,13 +35,34 @@ async function getSymbol(){
         console.log(error)
     }
 }
+var yValues = [500,1000,1500,2000,2500,3000];
+var xValues = [`Jan`,`Feb`,`Mar`,`Apr`,`May`,`Jun`,`Jul`,`Aug`,`Sep`,`Oct`,`Nov`,`Dec`];
+let prices=[]
+let dates=[]
 
 async function history(){
     try{
+        loader2.classList.add(`display`)
         const response = await fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/historical-price-full/${extractedSymbol}?serietype=line`)
-        const data = await response.json()
-        console.log(data)
+        const datae = await response.json()
+        prices.push(datae.historical.close)
+        dates.push(datae.historical.date)
         const ctx = document.getElementById('myChart');
+        const myChart = new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: dates,
+                datasets: [{
+                    fill: false,
+                    lineTension: 0,
+                    backgroundColor: "rgba(0,0,255,1.0)",
+                    borderColor: "rgba(0,0,255,0.1)",
+                    data: prices
+                }]
+            },
+            options: {}
+        });
+    loader2.remove(`display`)
     }
     catch(error){
         console.log(error)
--- js.js
+++ js.js
@@ -6,9 +6,9 @@ let btn=document.getElementById("button");
 async function present10(){
     try{
         loader.classList.add(`display`)
-        const response = await fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${input.value}&amp%3Blimit=10&amp%3Bexchange=NASDAQ`)
+        const response = await fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${input.value}&limit=10&amp%3Bexchange=NASDAQ`)
         const data = await response.json()
-
+console.log(data)
         for(let item of data){
             var link= `<a href="./company.html?symbol=${item.symbol}" target="_blank">${item.name} , ${item.symbol}<br /></a>`
             list.innerHTML+= link
